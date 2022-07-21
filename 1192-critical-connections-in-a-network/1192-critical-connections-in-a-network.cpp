b97a7503f86deeb5ffa5e21bfbea8d938684d017
aashish0909 <56581495+aashish0909@users.noreply.github.com>
2022-07-21 20:11:48 +0530

    Time: 1522 ms (5.03%), Space: 184.4 MB (35.14%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> ans;
+        vector<int> in(n, -1);
+        vector<int> low(n, -1);
+        vector<int> visited(n, 0);
+        int timer = 0;
+        vector<int> graph[n];
+        
+        for(auto i : connections) {
+            graph[i[0]].push_back(i[1]);
+            graph[i[1]].push_back(i[0]);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(graph, visited, in, low, i, -1, timer, ans);
+            }
+        }
+        
+        return ans;
+    }
+    
+    void dfs(vector<int> graph[], vector<int>& visited, vector<int>& in, vector<int>& low, int &node, int par, int &timer, vector<vector<int>>& ans) {
+        
+        visited[node] = 1;
+        in[node] = low[node] = timer++;
+        
+        for(auto it : graph[node]) {
+            if(it == par)
+                continue;
+            
+            if(!visited[it]) {
+                dfs(graph, visited, in, low, it, node, timer, ans);
+                low[node] = min(low[node], low[it]);
+                if(low[it] > in[node]) {
+                    vector<int> temp = {node, it};
+                    cout << node << ' ' << it;
+                    ans.push_back(temp);
+                }
+            }
+            else {
+                low[node] = min(low[node], in[it]);
+            }
+        }
+    }
+};
