9167a3368cb17ce8a62bc1af3daa0952c02fe4d6
aashish0909 <56581495+aashish0909@users.noreply.github.com>
2022-07-31 01:52:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (73.96%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,36 +1,21 @@
 class Solution {
-public:
-    int calc(string s, int i, vector<int>& dp) {
-        if(i >= s.size())
-            return 1;
-        
-        if(dp[i] != 0)
-            return dp[i];
-        
-        int ans = 0;
-        
-        if(s[i] != '0')
-            ans += calc(s, i + 1, dp);
-        
-        if(i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6'))
-            ans += calc(s, i + 2, dp);
-        
-        return dp[i] = ans;
-    
-    }
-    
+public:    
     int numDecodings(string s) {
-        vector<int> dp(s.size() + 1, 0);
+        int dp, dp1, dp2;
         int n = s.size();
-        dp[n] = 1;
+        dp = dp2 = 0;
+        dp1 = 1;
         
         for(int i = n - 1; i >= 0; i--) {
             if(s[i] != '0')
-                dp[i] += dp[i + 1];
+                dp += dp1;
             if(i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6'))
-                dp[i] += dp[i + 2];
+                dp += dp2;
+            dp2 = dp1;
+            dp1 = dp;
+            dp = 0;
         }
         
-        return dp[0];
+        return dp1;
     }
 };
