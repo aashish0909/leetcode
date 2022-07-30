d5e6d3fed839e89b789c8ba55f6701f221e8362a
aashish0909 <56581495+aashish0909@users.noreply.github.com>
2022-07-31 01:48:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (48.37%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -20,7 +20,17 @@ public:
     }
     
     int numDecodings(string s) {
-        vector<int> dp(s.size(), 0);
-        return calc(s, 0, dp);
+        vector<int> dp(s.size() + 1, 0);
+        int n = s.size();
+        dp[n] = 1;
+        
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] != '0')
+                dp[i] += dp[i + 1];
+            if(i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] <= '6'))
+                dp[i] += dp[i + 2];
+        }
+        
+        return dp[0];
     }
 };
