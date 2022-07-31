d0333a805884aebe16698ddfca37f0c07bfeb9cb
aashish0909 <56581495+aashish0909@users.noreply.github.com>
2022-07-31 17:41:57 +0530

    Time: 31 ms (96.76%), Space: 75.9 MB (20.66%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> dpp;
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(auto i : nums) sum += i;
+        for(auto i : nums)
+            sum += i;
+        int n = nums.size();
         
         if(sum % 2) return false;
         
-        vector<int> partition(sum / 2 + 1, false);
-        partition[0] = true;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            for(int j = sum / 2; j >= nums[i]; j--) {
-                partition[j] = partition[j] || partition[j - nums[i]];
-            }
-        }
-        
-        return partition[sum / 2];
+        vector<vector<int>> dp(n, vector<int>((sum / 2 )+ 1, -1));
+        return calc(nums, sum / 2, 0, dp);
+    }
+    
+    bool calc(vector<int>& nums, int sum, int i, vector<vector<int>>& dp) {
+        if(sum == 0)
+            return true;
+        if(sum < 0 || i >= nums.size())
+            return false;
+        if(dp[i][sum] != -1)
+            return dp[i][sum];
+        return dp[i][sum] = calc(nums, sum - nums[i], i + 1, dp) || calc(nums, sum, i + 1, dp);
     }
 };
